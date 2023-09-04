074774a0fd5fa67cd8008476b52046361d29c929
Dhruva <dhruvanayak1905@gmail.com>
2023-09-04 19:40:56 +0530

    few changes to carousel.jsx and hero.jsx

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Carousel = () => {
   return (
-    <div>
-    Carousel
-    {/* TODO */}
+    <div className='flex justify-center items-center'>
+      <img className='w-[700px] h-[400px]' src={"https://www.hostgator.com/blog/wp-content/uploads/2021/03/How-to-Make-a-Website-Quickly.jpg"}></img>
+
     </div>
   )
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='flex flex-col justify-center items-center'>
+      <p className='text-[40px] font-[montserrat] font-bold mt-[30px]'>I don’t know how to put</p>
+      <p className='text-[40px] font-[montserrat] font-black text-[#009EFD] '>NEXUS CLUB</p>
+      <div>
+      <button className='border-solid border-[1px] mx-[10px] px-[15px] rounded-[5px] text-black bg-white'>click me</button>
+      <button className='border-solid border-[1px] mx-[10px] px-[5px] rounded-[5px] bg-secondary'>click button</button>
+      </div>
+    </div>
   )
 }
 
