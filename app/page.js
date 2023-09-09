4900c7922e52b5573e0358270cf75ef127fac93c
Sanchay Shetty <shettysanchay@gmail.com>
2023-09-09 17:32:33 +0530

    rendering FromHeads components

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Projects from "../components/Projects";
 import Activities from "../components/Activities";
 import Comments from "../components/Comments";
 import Hero from "../components/Hero";
+import FromHeads from "../components/FromHeads"
 
 export default function Home() {
 	return (
@@ -19,6 +20,8 @@ export default function Home() {
 			<Projects />
 
 			<Comments />
+
+			<FromHeads/>
 		</main>
 	);
 }
