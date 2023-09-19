e9cbfd904772cdd8840e8067292945cd5ea74b61
Sarang Kumar <sarangkumar1578@gmail.com>
2023-09-19 22:34:27 +0530

    modified FromHeads

--- components/FromHeads.jsx
+++ components/FromHeads.jsx
@@ -4,85 +4,86 @@ import React from "react";
 const head_data = [
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1",
-		domain: "EVM",
+		name: "John Doe",
+		domain: "EVM 1",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibu",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget tortor ligula. In hac habitasse platea dictumst. Sed scelerisque aliquet arcu a pharetra.",
 	},
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1",
+		name: "Jane Smith",
 		domain: "EVM 2",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibusdam assumenda enim eius vel, est velit odit!Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibusdam assumenda enim eius vel, est velit odit!Lorem ipsum dolor sit quibu",
+			"Vivamus suscipit nisl vel nibh efficitur, id congue justo feugiat. Sed eget quam a nisl ultricies dignissim. Quisque sit amet purus lorem.",
 	},
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1",
+		name: "Alice Johnson",
 		domain: "EVM 3",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibu",
+			"Proin ullamcorper libero vel risus laoreet, a vulputate justo volutpat. Nunc auctor, ante nec bibendum eleifend, lectus augue convallis ex.",
 	},
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1",
+		name: "Bob Wilson",
 		domain: "EVM 4",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibu",
+			"Suspendisse a enim eget purus tincidunt elementum. Integer vel eros id purus fringilla posuere. Ut non volutpat justo, id efficitur tortor.",
 	},
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1",
+		name: "Emily Davis",
 		domain: "EVM 5",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, q Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibu",
+			"Fusce hendrerit facilisis metus, eget dignissim turpis consequat nec. In ac felis eu felis rhoncus posuere. Nullam convallis est id nunc elementum.",
 	},
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1",
+		name: "Michael Brown",
 		domain: "EVM 6",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibu",
+			"Maecenas vel quam at ex luctus consequat. Vestibulum id sapien non dui bibendum vehicula in eu metus. Sed ac urna non velit consectetur semper.",
 	},
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1",
+		name: "Olivia Wilson",
 		domain: "EVM 7",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibu",
+			"Donec volutpat ante id lectus consectetur euismod. Phasellus ultrices ex at quam eleifend, nec vehicula nisl fringilla. Vestibulum consectetur purus vel nisi.",
 	},
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1 8",
+		name: "Daniel Lee",
 		domain: "EVM 8",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibu",
+			"Pellentesque vel euismod erat, non mattis sapien. Morbi in leo a orci sollicixtudin volutpat. Vivamus auctor metus id quam vehicula, vel semper veli ellentesque vel euismod erat, non mattis sapien. Morbi in leo a orci sollicixtudin volutpat. Vivamus auctor metus id quam vehicula, vel semper velit ellentesque vel euismod erat, non mattis sapien. Morbi in leo a orci sollicixtudin volutpat. Vivamus auctor metus id quam vehicula, vel semper velit.",
 	},
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1",
+		name: "Sophia Clark",
 		domain: "EVM 9",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibu",
+			"Cras et ullamcorper urna. Vivamus bibendum quam nec tortor ultricies, vel iaculis justo efficitur. Curabitur suscipit enim id velit finibus, id dignissim sapien fermentum.",
 	},
 	{
 		imageUrl: "/images/project-icon/OFAAX40 12.png",
-		name: "Name 1",
-		domain: "EVM 9",
+		name: "William Adams",
+		domain: "EVM 10",
 		message:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaq orem ipsum dolor sit amet consectetur adipisicing elit. Incidunt hic ab veniam saepe repellendus eaque at voluptas molestiae praesentium porro nulla rem, quibu",
+			"Quisque vitae justo at augue luctus aliquet. Vestibulum sit amet sem vel eros varius dictum. Maecenas at sapien at augue bibendum vehicula.",
 	},
 ];
+
 
 export default function FromHeads() {
 	return (
 		<div className="phone:p-1 my-6 p-[0.5]">
-			<p className="text-2xl font-semibold font-montserrat  text-white text-transparent">
+				<h1 className="font-bold text-2xl font-montserrat my-3">
 				From Our
-				<span className="text-2xl  font-semibold font-montserrat my-4 text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
-					{" "}Heads
+				<span className="font-bold ml-2 bg-gradient-to-b from-primary to-secondary inline-block text-transparent bg-clip-text">
+					Heads
 				</span>
-			</p>
+			</h1>
 
 			<div className="xl:columns-4 lg:columns-3 columns-1 sm:columns-2 gap-4 mx-auto space-y-3 pb-28">
 				{/* <div className="bg-gray-200 break-inside-avoi"></div> */}
@@ -99,17 +100,17 @@ export default function FromHeads() {
 
 const Heads = (props) => {
 	return (
-		<article className="transition-colors break-inside-avoid  border-white/10 border-[1px] rounded-md p-4 hover:border-primary">
-			<div className="flex gap-2 bg bg-background font-montserrat">
+		<article className="transition-colors break-inside-avoid border-white/10 border-[1px] rounded-md p-4 hover:border-primary">
+			<div className="flex gap-2 bg-background font-montserrat">
 				<Image
 					height={100}
 					width={100}
 					src={props.imageUrl}
-					alt="Profile Pic"
+					alt={`${props.name}-thumbnail`}
 					className="h-12 w-12"
 				/>
-				<article className="hidden md:flex flex-col">
-					<h2 className="text-base font-bold md:text-lg font-poppins">{props.name}</h2>
+				<article className="flex flex-col">
+					<h2 className="text-base font-bold font-poppins">{props.name}</h2>
 					<p className="text-xs font-medium tracking-wide font-poppins text-text-secondary">
 						{props.domain}
 					</p>
