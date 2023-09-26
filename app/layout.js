5a6a5877014fb6fe359da0bfdf4a48a57a5fe139
Sarang Kumar <sarangkumar1578@gmail.com>
2023-09-26 12:24:01 +0530

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
 				className={`${inter.className} bg-background text-text-primary font-inter`}
 			>
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
