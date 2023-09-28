bf7bb56f35a931380a17236335a9676b1b996e0d
Sarang Kumar <sarangkumar1578@gmail.com>
2023-09-28 09:24:29 +0530

    optimised the file and folder structure

--- components/data.js
+++ components/data.js
@@ -1,4 +1,4 @@
-const projectData = [
+export const projectData = [
 	{
 		id: 1,
 		projectThumbnail: "/images/project-icon/notevault-thumbnail.png",
@@ -97,4 +97,101 @@ const projectData = [
 	},
 ];
 
-export default projectData;
+export const activityData = [
+	{
+		id: 1,
+		images: [
+			"/images/events/unsplash_-HIiNFXcbtQ.png",
+			"/images/events/unsplash_fT49QnFucQ8.png",
+			"/images/events/unsplash_ioJBsYQ-pPM.png",
+			"/images/events/unsplash_l5Tzv1alcps.png",
+			"/images/events/unsplash_Wa9ilX9XYOI.png",
+			"/images/events/unsplash_XtUd5SiX464.png",
+		],
+		date: new Date("Sep 9, 2023 9:00:00"),
+		summary:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consectetur nemo aliquam nostrum autem vitae, cupiditate quia pariatur quis",
+		likes: 2,
+		eventName: "NexHunt",
+	},
+	{
+		id: 2,
+		images: [
+			"/images/events/unsplash_-HIiNFXcbtQ.png",
+			"/images/events/unsplash_fT49QnFucQ8.png",
+			"/images/events/unsplash_ioJBsYQ-pPM.png",
+			"/images/events/unsplash_l5Tzv1alcps.png",
+			"/images/events/unsplash_Wa9ilX9XYOI.png",
+			"/images/events/unsplash_XtUd5SiX464.png",
+		],
+		date: new Date("Sep 9, 2023 9:00:00"),
+		summary:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consectetur nemo aliquam nostrum autem vitae, cupiditate quia pariatur quis",
+		likes: 3,
+		eventName: "NeuroNex",
+	},
+	{
+		id: 3,
+		images: [
+			"/images/events/unsplash_-HIiNFXcbtQ.png",
+			"/images/events/unsplash_fT49QnFucQ8.png",
+			"/images/events/unsplash_ioJBsYQ-pPM.png",
+			"/images/events/unsplash_l5Tzv1alcps.png",
+			"/images/events/unsplash_Wa9ilX9XYOI.png",
+			"/images/events/unsplash_XtUd5SiX464.png",
+		],
+		date: new Date("Sep 9, 2023 9:00:00"),
+		summary:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consectetur nemo aliquam nostrum autem vitae, cupiditate quia pariatur quis",
+		likes: 9,
+		eventName: "NexHunt",
+	},
+	{
+		id: 4,
+		images: [
+			"/images/events/unsplash_-HIiNFXcbtQ.png",
+			"/images/events/unsplash_fT49QnFucQ8.png",
+			"/images/events/unsplash_ioJBsYQ-pPM.png",
+			"/images/events/unsplash_l5Tzv1alcps.png",
+			"/images/events/unsplash_Wa9ilX9XYOI.png",
+			"/images/events/unsplash_XtUd5SiX464.png",
+		],
+		date: new Date("Sep 9, 2023 9:00:00"),
+		summary:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consectetur nemo aliquam nostrum autem vitae, cupiditate quia pariatur quis",
+		likes: 9,
+		eventName: "NeuroNex",
+	},
+	{
+		id: 5,
+		images: [
+			"/images/events/unsplash_-HIiNFXcbtQ.png",
+			"/images/events/unsplash_fT49QnFucQ8.png",
+			"/images/events/unsplash_ioJBsYQ-pPM.png",
+			"/images/events/unsplash_l5Tzv1alcps.png",
+			"/images/events/unsplash_Wa9ilX9XYOI.png",
+			"/images/events/unsplash_XtUd5SiX464.png",
+		],
+		date: new Date("Sep 9, 2023 9:00:00"),
+		summary:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consectetur nemo aliquam nostrum autem vitae, cupiditate quia pariatur quis",
+		likes: 4,
+		eventName: "NexHunt",
+	},
+	{
+		id: 6,
+		images: [
+			"/images/events/unsplash_-HIiNFXcbtQ.png",
+			"/images/events/unsplash_fT49QnFucQ8.png",
+			"/images/events/unsplash_ioJBsYQ-pPM.png",
+			"/images/events/unsplash_l5Tzv1alcps.png",
+			"/images/events/unsplash_Wa9ilX9XYOI.png",
+			"/images/events/unsplash_XtUd5SiX464.png",
+		],
+		date: new Date("Sep 9, 2023 9:00:00"),
+		summary:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consectetur nemo aliquam nostrum autem vitae, cupiditate quia pariatur quis",
+		likes: 2,
+		eventName: "NeuroNex",
+	},
+];
