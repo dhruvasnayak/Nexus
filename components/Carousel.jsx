c82fbf7d214aa3a2fa33e5832ce013b8b034c5f6
Dhruva <dhruvanayak1905@gmail.com>
2023-09-30 20:58:27 +0530

    autorotate

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,40 +1,43 @@
-"use client"
+'use client'
 
-import React from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { useState, useEffect, useRef } from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
-import './Carousel.css'
+import './Carousel.css';
 
 const Carouseld = () => {
-  const images = [{
-    id: 1,
-    src: "/images/events/unsplash_-HIiNFXcbtQ.png",
-    alt: "Image 1"
-  },
-  {
-    id: 2,
-    src: "/images/events/unsplash_fT49QnFucQ8.png",
-    alt: "Image 2 "
-  },
-  {
-    id: 3,
-    src: "/images/events/unsplash_ioJBsYQ-pPM.png",
-
-    alt: "Image 3"
-  },
-  {
-    id: 4,
-    src: "/images/events/unsplash_XtUd5SiX464.png",
-
-    alt: "Image 4"
-  },
-  {
-    id: 5,
-    src: "/images/events/unsplash_Wa9ilX9XYOI.png",
-
-    alt: "Image 5"
-  }
+  const images = [
+    {
+      id: 1,
+      src: '/images/events/unsplash_-HIiNFXcbtQ.png',
+      alt: 'Image 1',
+    },
+    {
+      id: 2,
+      src: '/images/events/unsplash_fT49QnFucQ8.png',
+      alt: 'Image 2',
+    },
+    {
+      id: 3,
+      src: '/images/events/unsplash_ioJBsYQ-pPM.png',
+      alt: 'Image 3',
+    },
+    {
+      id: 4,
+      src: '/images/events/unsplash_XtUd5SiX464.png',
+      alt: 'Image 4',
+    },
+    {
+      id: 5,
+      src: '/images/events/unsplash_Wa9ilX9XYOI.png',
+      alt: 'Image 5',
+    },
   ];
+
+  const [autoPlayEnabled, setAutoPlayEnabled] = useState(true);
+  const [temp, setTemp] = useState(false);
+  const carouselRef = useRef(null);
+
   const settings = {
     dots: true,
     autoFocus: false,
@@ -43,21 +46,40 @@ const Carouseld = () => {
     showStatus: false,
     transitionTime: 700,
     useKeyboardArrows: true,
-    interval:1300,
-  }
+    interval: 1300,
+    autoPlay: autoPlayEnabled,
+    infiniteLoop: true,
+    onChange: (index) => {
+      if (temp && index==0) {
+        setAutoPlayEnabled(false);
+      } else {
+        setAutoPlayEnabled(true);
+        setTemp(true);
+      }
+    },
+  };
+
   return (
-    <div className='flex justify-center flex-col items-center'>
+    <div className="flex justify-center flex-col items-center">
       <div className="carousel-container">
-        <Carousel {...settings} className='mt-6 rounded'>
+        <Carousel
+          ref={carouselRef}
+          {...settings}
+          className="mt-6"
+        >
           {images.map((item) => (
-            <div key={item.id}>
-              <img className='h-[450px] rounded-2xl' src={item.src} alt={item.alt} />
+            <div key={item.id} className="">
+              <img
+                className="h-[400px] sm:h-[500px] rounded-2xl"
+                src={item.src}
+                alt={item.alt}
+              />
             </div>
           ))}
         </Carousel>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Carouseld;
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ import { Button } from './Button';
 
 const Hero = () => {
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col justify-center items-center my-10'>
       <p className='text-[40px] sm:text-[50px] leading-10 font-montserrat font-bold'>I don&apos;t know how to put</p>
       <p className='text-[50px] leading-[65px] font-montserrat font-black text-[#009EFD] '>NEXUS CLUB</p>
       <div className='mt-5'>
